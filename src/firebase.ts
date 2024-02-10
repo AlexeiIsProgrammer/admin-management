91f1a27cd7b79f7d95b98ace451aade2f1f8e103
Alexei <avenge.now.r@mail.ru>
2024-02-10 12:46:31 +0300

    feat: add registration and authorization logic

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,11 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDEZgeBkZa8rSPStUjFxZHZho41u4snseA',
@@ -14,3 +19,50 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+const registerWithEmailAndPassword = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const { user } = res;
+    const status = 'active';
+    const registrationDate = Date.now();
+    const lastLoginDate = registrationDate;
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      name,
+      email,
+      registrationDate,
+      lastLoginDate,
+      status,
+      authProvider: 'local',
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      alert(err.message);
+    }
+  }
+};
+
+const logInWithEmailAndPassword = async (email: string, password: string) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    if (err instanceof Error) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      alert(err.message);
+    }
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export { registerWithEmailAndPassword, logInWithEmailAndPassword, logout };
